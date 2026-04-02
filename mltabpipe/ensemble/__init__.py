50f8453e331de59d8082f50f2a9d4d66bb6d382d
Michael Y. Qiu <altmickymike@outlook.com>
2026-04-02 10:13:15 +0800

    Auto import param tuning func

--- mltabpipe/ensemble/__init__.py
+++ mltabpipe/ensemble/__init__.py
@@ -1,4 +1,7 @@
-from .gbdt import train_xgb_model, train_lgbm_model, train_cb_model
+from .gbdt import (
+    train_xgb_model, train_lgbm_model, train_cb_model,
+    tune_xgb_hyperparameters, tune_lgbm_hyperparameters, tune_cb_hyperparameters
+)
 from .rf import train_rf_model
 from .stacker import train_stacker
 from .ridge import train_ridge_model
